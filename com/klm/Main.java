ab1031d62f4a9e45a06e8f2a97b7b1ecbf8ef751
Kyle <kyelmo8@gmail.com>
2020-09-16 08:48:11 -0700

    Testing out branches and PRs within IntelliJ

--- com/klm/Main.java
+++ com/klm/Main.java
@@ -1,12 +1,9 @@
 package com.klm;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Testing out branching from within IntelliJ");
 
     }
 }
